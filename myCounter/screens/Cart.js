8bf00b98d0fccad2d087b3d307c4eccf50ed66bd
SafaeQ <safaedani009@gmail.com>
2022-06-07 10:26:15 +0100

    🎨 initial project

--- myCounter/screens/Cart.js
+++ myCounter/screens/Cart.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, reset, increaseByAmount } from './counterSlice'
+import { StyleSheet, Text, View, FlatList, Dimensions, ActivityIndicator, Pressable, Image } from 'react-native';
+import { decrement, increment, reset, increaseByAmount } from './CartSlice'
 
 const CounterView = () => {
     const count = useSelector(state => state.counter.count)
     const dispatch = useDispatch()
+
   return (
-    <div>
-        <h3>Count : {count} </h3>
-        <button onClick={ () => {dispatch(increment())} } >Increment</button>
-        <button onClick={ () => {dispatch(decrement())} } >Decrement</button>
-    </div>
+    <>
+        <View>
+            <Text>Count : {count} </Text>
+            <button onClick={ () => {dispatch(increment())} } >Increment</button>
+            <button onClick={ () => {dispatch(decrement())} } >Decrement</button>
+        </View>
+    </>
   )
 }
 
